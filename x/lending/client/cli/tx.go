573181c2d23fb03c09c4e79aebb013681decc66f
Ben <benjaminwolfaardt@gmail.com>
2021-03-23 05:11:41 +0100

    Up to the end of chapter 6 working

--- x/lending/client/cli/tx.go
+++ x/lending/client/cli/tx.go
@@ -99,6 +99,7 @@ func payDebtCmdFunc(cmd *cobra.Command, args []string, cdc *codec.Codec) error {
 
 func createDebtCmd(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
+		Use:   "create [ID] [amount] [creditor]",
 		Short: "Creates a debt that should be collected",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
